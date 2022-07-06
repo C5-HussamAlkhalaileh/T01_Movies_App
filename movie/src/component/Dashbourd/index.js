38fbba8eea6ce6ec09f7d12293bcaa16ed70dcf8
HussamAlkhalaileh <alkhalaileh.hussam@gmail.com>
2022-07-06 22:02:02 +0300

    create and start dashbourd page

--- movie/src/component/Dashbourd/index.js
+++ movie/src/component/Dashbourd/index.js
@@ -1,2 +1,25 @@
-import { useState ,use } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "./App.css";
+
+
+const Dashbourd =()=>{
+
+    const [Dashbourd,serDashbourd]=useState("");
+
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=1bfa430aada4409bfa6a3c5528128e8a`).then((res) => {
+          setUsers(res.data);
+        });
+    }, []);
+
+
+return (
+    <div>
+
+    </div>
+)
+
+}
+
+export default Dashbourd
