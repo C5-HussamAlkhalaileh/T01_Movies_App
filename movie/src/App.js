f29eee1413aa5efa717ac029022728e9e47ff70a
HussamAlkhalaileh <alkhalaileh.hussam@gmail.com>
2022-07-06 21:44:20 +0300

    add new component  and update

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
-import Homepage from '../home';
 
 function App() {
   return (
  
-   <Routes>
-{/*home */}
-<Route path="/" element={<Home />} />
-{/*faviarote */}
-<Route path="/" element={<Home />} />
-{/*details */}
-<Route path="/" element={<Home />} />
- 
+    <div className="App">
 
-   </Routes>
+    </div>
+
  
   );
 }
 
 export default App;
+
+
+//{/*<Routes>
+//{/*home */}
+//<Route path="/" element={<Home />} />
+//{/*faviarote */}
+//<Route path="/" element={<faviarote />} />
+//{/*details */}
+//<Route path="/" element={<details />} />
+ 
+
+//   </Routes>*/}
---
+++ movie/src/component/Dashbourd/index.js
@@ -0,0 +1,2 @@
+import { useState ,use } from "react";
+import axios from "axios";
