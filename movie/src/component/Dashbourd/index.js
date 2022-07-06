d991d43294ec65a4caf1c360d637b6749bd2f319
HussamAlkhalaileh <alkhalaileh.hussam@gmail.com>
2022-07-06 23:16:32 +0300

    add some style to dashbour

--- movie/src/component/Dashbourd/index.js
+++ movie/src/component/Dashbourd/index.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css";
+import "./style.css";
 
 
 const Dashbourd =()=>{
 
-    const [Dashbourd,serDashbourd]=useState("");
+    const [Dashbourd,setDashbourd]=useState("");
 
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=1bfa430aada4409bfa6a3c5528128e8a`)
         .then((res) => {
-            serDashbourd(res.data);
+            setDashbourd(res.data.results);
+
         })
          
     }, []);
@@ -18,6 +19,25 @@ const Dashbourd =()=>{
 
 return (
     <div>
+        <button> Home</button>
+        <button> Faviarote</button>
+        <h1>Movies</h1>
+
+        {/*{posts.map((post, i) => {
+          return (
+            <div>
+               <img>{backdrop_path}</img> 
+             <h1>{title}</h1>
+              <p>{genre_ids}</p>
+              <p>{popularity}</p>
+              <p>{vote_count}</p>
+            </div>
+          );
+        })}
+        */}
+
+
+       
 
     </div>
 )
--- movie/src/component/Details/index.js
+++ movie/src/component/Details/index.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./App.css";
+//import React, { useState, useEffect } from "react";
+//import axios from "axios";
+//import "./App.css";
 
 
-const Deatails =()=>{
+//const Deatails =()=>{
 
-    const [Deatails,setDeatails]=useState("");
+//    const [Deatails,setDeatails]=useState("");
 
-    useEffect(() => {
-        axios.get("https://api.themoviedb.org/3/movie/475557?language=en-US&api_key=1bfa430aada4409bfa6a3c5528128e8a")
-        .then((res) => {
-            serDashbourd(res.data);
-        })
+//    useEffect(() => {
+//        axios.get("https://api.themoviedb.org/3/movie/475557?language=en-US&api_key=1bfa430aada4409bfa6a3c5528128e8a")
+//        .then((res) => {
+//            serDashbourd(res.data);
+//        })
          
-    }, []);
+//    }, []);
 
 
-return (
-    <div>
+//return (
+//    <div>
 
-    </div>
-)
+//    </div>
+//)
 
-}
+//}
 
-export default Deatails
+//export default Deatails
